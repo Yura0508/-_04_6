f91030f17f1f1f5da2f0221b0ddb09c224e3868e
Yura <urijkisil2007@gmail.com>
2024-09-26 00:52:05 +0300

    master1 commit

---
+++ lab_04_6/lab_04_6.cpp
@@ -0,0 +1,64 @@
+﻿// lab_04_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double P, S;
+	int k, i;
+	P = 1;
+	k = 1;
+	while (k <= 20)
+	{
+		S = 0;
+		i = 1;
+		while (i <= k)
+		{
+			S += 1.0 * i * i;
+			i++;
+		}
+		P *= S / (1 + S);
+		k++;
+	}
+	cout << P << endl;
+	P = 1;
+	k = 1;
+	do {
+		S = 0;
+		i = 1;
+		do {
+			S += 1.0 * i * i;
+			i++;
+		} while (i <= k);
+		P *= S / (1 + S);
+		k++;
+	} while (k <= 20);
+	cout << P << endl;
+	P = 1;
+	for (k = 1; k <= 20; k++)
+	{
+		S = 0;
+		for (i = 1; i <= k; i++)
+		{
+			S += 1.0 * i * i;
+		}
+		P *= S / (1 + S);
+	}
+	cout << P << endl;
+	P = 1;
+	for (k = 20; k >= 1; k--)
+	{
+		S = 0;
+		for (i = k; i >= 1; i--)
+		{
+			S += 1.0 * i * i;
+		}
+		P *= S / (1 + S);
+	}
+	cout << P << endl;
+	return 0;
+
+}
